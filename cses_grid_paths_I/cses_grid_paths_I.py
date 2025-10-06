298c697223ed7558893b0b6ac7e0e3a8feb38a6e
Giricz Vince <giricz@inf.u-szeged.hu>
2025-10-06 17:03:44 +0200

    cses grid paths I feladat megoldás

---
+++ cses_grid_paths_I/cses_grid_paths_I.py
@@ -0,0 +1,29 @@
+n = int(input())
+grid = []
+MOD = 10 ** 9 + 7
+
+for i in range(n):
+    grid.append([it == '.' for it in input().strip()])
+
+dp = [[0] * n for _ in range(n)]
+
+row = True
+col = True
+for i in range(n):
+    if row and grid[0][i]:
+        dp[0][i] = 1
+    if col and grid[i][0]:
+        dp[i][0] = 1
+
+    if not grid[0][i]:
+        row = False
+    if not grid[i][0]:
+        col = False
+
+
+for i in range(1, n):
+    for j in range(1, n):
+        if grid[i][j]:
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD
+
+print(dp[-1][-1])
